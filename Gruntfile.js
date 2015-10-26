3e36b322be4aaf841ae0d8a8d074365d8a330549
Evgeny Artamonov <evzheniy@ya.ru>
2015-10-26 23:03:59 +0300

    add build

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,10 +6,52 @@ module.exports = function(grunt) {
   var config = {
     pkg: grunt.file.readJSON('package.json'),
 
+    browserSync: {
+      bsFiles: {
+          src : './css/*.css'
+      },
+      options: {
+          watchTask: true,
+          server: {
+              baseDir: "./"
+          }
+      }
+    },
+    clean: {
+      build: ["build"]
+    },
+
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "",
+          src: [
+            "img/**",
+            "js/**",
+            "index.html",
+            "*.html",
+          ],
+          dest: "build"
+        }]
+      }
+    },
+
     sass: {
       style: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          'build/css/style.css': 'sass/style.scss'
+        }
+      }
+    },
+
+    cmq: {
+      options: {
+        log: false
+      },
+      your_target: {
+        files: {
+          'build/css/style.css': ['build/css/style.css']
         }
       }
     },
@@ -21,19 +63,86 @@ module.exports = function(grunt) {
         ]
       },
       style: {
-        src: 'css/*.css'
+        src: 'build/css/*.css'
       }
     },
 
-    browserSync: {
-      bsFiles: {
-          src : './css/*.css'
-      },
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'build/css',
+          src: ['*.css', '!*.min.css'],
+          dest: 'build/css',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png,jpg,gif,svg}"] 
+        }]
+      }
+    },
+
+    htmlmin: {
       options: {
-          watchTask: true,
-          server: {
-              baseDir: "./"
-          }
+        removeComments: true,
+        collapseWhitespace: true,
+        collapseBooleanAttributes: true,
+        caseSensitive: true,
+        keepClosingSlash: false
+      }, 
+      html: {
+        files: {
+          "build/index.min.html": "build/index.html",
+          "build/form.min.html": "build/form.html",
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';'
+      },
+      dist: {
+        src: ['build/js/*.js'],
+        dest: 'build/js/script.js'
+      }
+    },
+
+    uglify: {
+      my_target: {
+        files: {
+          'build/js/script.min.js': ['build/js/script.js']
+        }
+      }
+    },
+
+    replace: {
+      build: {
+        options: {
+          patterns: [{
+            match: "script.js",
+            replacement: "script.min.js"
+          },
+          {
+            match: "style.css",
+            replacement: "style.min.css"
+          }]
+        },
+        files: [{
+          expand: true,
+          src: [
+            "build/*.html"
+          ]
+        }]
       }
     },
 
@@ -51,5 +160,6 @@ module.exports = function(grunt) {
 
   config = require('./.gosha')(grunt, config);
   grunt.registerTask('default', ['browserSync', 'watch']);
+  grunt.registerTask('build', ['clean', 'copy', 'sass', 'cmq', 'postcss', 'cssmin', 'imagemin', 'htmlmin', 'concat', 'uglify', 'replace']);
   grunt.initConfig(config);
 };
